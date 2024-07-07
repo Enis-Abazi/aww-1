37dd7815cc7cff01241f0d63f180328ac551bb1e
dionabazi <dionabazi5@gmail.com>
2024-07-07 20:12:38 +0200

    enis/wip

---
+++ Script/pricing.js
@@ -0,0 +1,5 @@
+const firstName = document.querySelector("#fname");
+const lastName = document.querySelector("#lname");
+const email = document.querySelector("#email");
+const projectNameFree = document.querySelector("#projectNameFree");
+const projectNameAnnual = document.querySelector('#projectNameAnnual');
